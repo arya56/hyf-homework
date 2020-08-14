e2f9eec26280bdf23eaf8c7faea0b6f43dbb0edc
arya56 <arya56@yahoo.com>
2020-08-14 20:44:40 +0200

    Implement feedback

--- javascript/javascript1/week1/Ez_Namey.js
+++ javascript/javascript1/week1/Ez_Namey.js
@@ -2,4 +2,4 @@ const firstWords = [ "Easy" , "Awesome" , "Corporate" , "Highly" , "Brownish", "
 const secondWords = [ "Golden" , "Silver" , "Ultimate" , "Absolute" , "Sky" , "Earth" , "Planet", "Panda", "Bear" , "Wolves"];
 let startupName= "";
 startupName = firstWords[Math.floor(Math.random() * 10) + 0] +" "+secondWords[Math.floor(Math.random() * 10) + 0];
-console.log("The startup: "+`"${startupName}"`+" contains " +startupName.length+" characters");
+console.log(`The startup: ${startupName} contains ${startupName.length} characters`);
