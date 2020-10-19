0168ed98d154b69e156c99b7d3611648f8767aa0
arya56 <arya56@yahoo.com>
2020-10-19 14:58:49 +0200

    Implement task

---
+++ javascript/javascript3/week3/Art/art_Using.js
@@ -0,0 +1,64 @@
+// draw circle
+
+const canvas = document.getElementById('myCanvas');
+const pageWidth = window.innerWidth;
+const pageHeight = window.innerHeight;
+canvas.width = pageWidth;
+canvas.height = pageHeight;
+
+class Circle {
+  constructor(x, y, r, startAngle, endAngle, fillColor) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.startAngle = startAngle;
+    this.endAngle = endAngle;
+    this.fillColor = fillColor;
+  }
+
+  draw() {
+    const ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.r, this.startAngle, this.endAngle * Math.PI);
+    ctx.fillStyle = this.fillColor;
+    ctx.fill();
+    ctx.stroke();
+  }
+}
+// Test Circle Class
+const c1 = new Circle(50, 50, 20, 0, 2, '#0000ff');
+c1.draw();
+
+// draw a circle every 100milisecond in random position with width and height of the screen
+setInterval(() => {
+  const newCircle = new Circle
+  (ranodmizer(pageWidth),ranodmizer(pageHeight),ranodmizer(pageWidth / 5),0,2,getRandomColor());
+  newCircle.draw();
+}, 100);
+
+// mouse move  creatin circle aroun mouse cursor
+const body = document.querySelector('body');
+body.addEventListener('mousemove', e => {
+  x = e.offsetX;
+  y = e.offsetY;
+  setInterval(() => {
+    const newCircle = new Circle(x + 10, y + 10, x / 5, 0, 2, getRandomColor());
+    newCircle.draw();
+  }, 1000);
+});
+
+//  produce random number less than X
+
+function ranodmizer(x) {
+  return Math.floor(Math.random() * x);
+}
+
+//   produce random color
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
