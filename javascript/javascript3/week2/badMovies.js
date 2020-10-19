772db3de47c86aa8a493a38c9aed944137874b03
arya56 <arya56@yahoo.com>
2020-10-19 13:41:20 +0200

    apply reviews

--- javascript/javascript3/week2/badMovies.js
+++ javascript/javascript3/week2/badMovies.js
@@ -1,14 +1,11 @@
 const movie_Api = 'https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json'
 const badTitlesAfter2000 = []
-let ali = []
 
-async function badMovies(){
+async function getBadMovies(){
     try{
             const movieResponse = await fetch(movie_Api)
             const movies = await movieResponse.json()
-
             const badRated =  movies.filter(movie=>movie.rating < 3)
-
             const badRatedAfter2000 = badRated.filter(movie => movie.year >= 2000)
         // this is extra an Array of bad movies after2000 titles
             badRatedAfter2000.forEach(movie => badTitlesAfter2000.push(movie.title))
@@ -22,4 +19,4 @@ async function badMovies(){
     }
 }
 
-badMovies()
+getBadMovies()
