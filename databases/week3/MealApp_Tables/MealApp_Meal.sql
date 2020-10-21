d5e4de1dacc2dbb6e23a0b04471c55cbf269a3e6
arya56 <arya56@yahoo.com>
2020-10-21 13:27:57 +0200

    Implement Tasks

---
+++ databases/week3/MealApp_Tables/MealApp_Meal.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Linux (x86_64)
+--
+-- Host: localhost    Database: MealApp
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Meal`
+--
+
+DROP TABLE IF EXISTS `Meal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Meal` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `description` text,
+  `location` varchar(255) DEFAULT NULL,
+  `when_create` datetime DEFAULT NULL,
+  `max_reservations` int DEFAULT NULL,
+  `price` decimal(10,0) DEFAULT NULL,
+  `created_date` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Meal`
+--
+
+LOCK TABLES `Meal` WRITE;
+/*!40000 ALTER TABLE `Meal` DISABLE KEYS */;
+INSERT INTO `Meal` VALUES (1,'Smørrebrød','traditional Scandinavian open-faced sandwich','odensevej 123','2017-09-03 17:14:55',5,110,'2017-09-04'),(2,'Moroccan Tajine','typical home-style dishes that are seasonal and often feature Casablanca','Risøvej 123','2019-09-03 17:14:55',15,130,'2019-09-04'),(3,'Fresh Pasta','Sprinkle the salt and drizzle the olive oil on top of the eggs','københavnvej 324','2020-10-03 10:14:55',6,150,'2020-10-04'),(4,'French taste','chicken roasted','Frederikssundsvej 12','2020-10-13 11:14:55',4,120,'2020-10-14'),(5,'Farsbrød','Hakket svinekød','Emdrupvej 5','2020-10-14 13:14:55',2,115,'2020-10-15'),(6,'Chiken Massala','typical indian dishes with full of spices','Ballerupvej 43','2020-10-20 22:00:11',10,125,'2020-09-04'),(8,'frikadeller','typical Danish meatball','Risøvej 123','2020-10-20 17:14:55',15,80,'2019-09-04');
+/*!40000 ALTER TABLE `Meal` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-10-21 13:19:35
---
+++ databases/week3/MealApp_Tables/MealApp_Review.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Linux (x86_64)
+--
+-- Host: localhost    Database: MealApp
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Review`
+--
+
+DROP TABLE IF EXISTS `Review`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Review` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `description` text,
+  `meal_id` int DEFAULT NULL,
+  `stars` int DEFAULT NULL,
+  `created_date` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `meal_id` (`meal_id`),
+  CONSTRAINT `Review_ibfk_1` FOREIGN KEY (`meal_id`) REFERENCES `Meal` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Review`
+--
+
+LOCK TABLES `Review` WRITE;
+/*!40000 ALTER TABLE `Review` DISABLE KEYS */;
+INSERT INTO `Review` VALUES (1,'Excellent','Fresh ingredients',1,5,NULL),(2,'tastefull','high level of food',1,4,NULL),(3,'POOR','old veg',2,1,NULL),(4,'Lovely','nice design and decoration',3,4,NULL),(5,'low','not bad but could be better in quality',4,2,NULL),(6,'very good','original taste',5,4,NULL),(8,'Very Good','Perfect',2,5,NULL);
+/*!40000 ALTER TABLE `Review` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-10-21 13:19:35
