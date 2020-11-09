80f793f48d6c218cd26f497be023f158198f37ca
arya56 <arya56@yahoo.com>
2020-11-09 16:26:54 +0100

    some comment and changes not final

--- nodejs/week2/warmup/index.js
+++ nodejs/week2/warmup/index.js
@@ -1,48 +1,56 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => res.send("nodejs week2 homework"));
+app.get('/', (req, res) => res.send('nodejs week2 homework'));
 
-
-
-app.get("/numbers/add", (req,res)=>{
-    const number1 = parseInt(req.query.first);
-    const number2 = parseInt(req.query.second);
-    if(req.query.first && req.query.second){
-        if (!isNaN(number1) && !isNaN(number2)){
-            const sum = number1 + number2 ;
-            res.send(`Sum of ${number1} and ${number2} is ${sum}`)
-            }
-        else{
-            res.send(`Please write numbers on the query`)
-        }
+app.get('/numbers/add', (req, res) => {
+  const number1 = parseInt(req.query.first);
+  const number2 = parseInt(req.query.second);
+  if (req.query.first && req.query.second) {
+    if (!isNaN(number1) && !isNaN(number2)) {
+      const sum = number1 + number2;
+      res.send(`Sum of ${number1} and ${number2} is ${sum}`);
+    } else {
+      res.send(`Please write numbers on the query`);
     }
-    else {
-        res.send(`Please write a query in this format numbers/add?first=11&second=12`)
-    }
-
+  } else {
+    res.send(
+      `Please write a query in this format numbers/add?first=11&second=12`
+    );
+  }
 });
-
-app.get("/numbers/multiply/:first/:second", (req,res)=>{
-    const number1 = parseInt(req.params.first);
-    const number2 = parseInt(req.params.second);
-    if (req.params.first && req.params.second){
-        if(!isNaN(number1) && !isNaN(number2)){
-            const multiply = number1 * number2 ;
-            res.send(`Multiply of ${number1} to ${number2} is ${multiply} `)
-        }
-        else{
-            res.send(`Please write numbers on the query`)
-        }
+app.get('/numbers/multiply/:first/:second', (req, res) => {
+  const number1 = parseInt(req.params.first);
+  const number2 = parseInt(req.params.second);
+  if (req.params.first && req.params.second) {
+    if (!isNaN(number1) && !isNaN(number2)) {
+      const multiply = number1 * number2;
+      res.send(`Multiply of ${number1} to ${number2} is ${multiply} `);
+    } else {
+      res.send(`Please write numbers on the query`);
     }
-    else{
-        res.send(`Please write a query in this format /numbers/multiply/11/12`)
-    }
+  } else {
+    res.send(`Please write a query in this format /numbers/multiply/11/12`);
+  }
 });
-app.get("/numbers/multiply/:first", (req, res) => {
-    res.send(`Please write parameters in this format /numbers/multiply/11/12`)
+app.get('/numbers/multiply/:first', (req, res) => {
+  res.send(`Please write parameters in this format /numbers/multiply/11/12`);
 });
-app.get("/numbers/multiply/", (req, res) => {
-    res.send(`parameters should add in this format /numbers/multiply/11/12`)
+app.get('/numbers/multiply/', (req, res) => {
+  res.send(`parameters should add in this format /numbers/multiply/11/12`);
 });
 app.listen(3000, () => console.log(`Calculator:listening on port 3000`));
+
+// function checker(numberA, numberB) {
+//   if (numberA & numberB) {
+//     firstNum = parseInt(numberA);
+//     secondNum = parseInt(numberB);
+//     if(!isNaN(firstNum) && !isNaN(secondNum)){
+//         return firstNum,secondNum;
+//     }
+//     else{
+//         return `Please write numbers on the query`
+//     }
+//   }
+// }
+
